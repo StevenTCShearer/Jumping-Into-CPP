5def2ef68ee6017726b04a7d6e91808610bc2e93
= <StevenTCShearer@hotmail.co.uk>
2018-10-16 00:30:08 +0100

    Chapter 2 of the book

---
+++ Chapter2/Chapter2Task1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    string firstName, secondName;
+	cout << "Please enter your first name" << endl;
+    cin >> firstName;
+    cout << "Please enter your second name" << endl;
+    cin >> secondName;
+    cout << "Hi " << firstName << " " << secondName << endl;
+	return 0;
+}
---
+++ Chapter2/Chapter2Task2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int number1, number2;
+	cout << "Please choose a number" << endl;
+    cin >> number1;
+    cout << "Please choose a second number" << endl;
+    cin >> number2;
+    cout << number1 << " + " << number2 << " = " << number1 + number2 << endl;
+	return 0;
+}
