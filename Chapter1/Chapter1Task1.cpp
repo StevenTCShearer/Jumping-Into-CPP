90d54a2067a4eff2e9de35f590a4126217218f7d
= <StevenTCShearer@hotmail.co.uk>
2018-10-15 23:33:12 +0100

    Starting Block and Chapter 1

---
+++ Chapter1/Chapter1Task1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	cout << "Steven Shearer" << endl;
+    return 0;
+}
---
+++ Chapter1/Chapter1Task2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	cout << "This \n is\n a\n list\n of\n my\n friends" << endl;
+	return 0;
+}
